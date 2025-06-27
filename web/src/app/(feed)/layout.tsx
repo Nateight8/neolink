d73ec8d1e3e8324bd155f9ab510a5fcea1041b90
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-06-27 16:20:07 +0100

    chore: commit all staged changes

--- web/src/app/(feed)/layout.tsx
+++ web/src/app/(feed)/layout.tsx
@@ -2,7 +2,6 @@ import type React from "react";
 import "@/app/globals.css";
 import { GameRoomsSidebar } from "@/components/feed/layout/right-sidebar";
 import LeftPannel from "@/components/feed/layout/left-pannel";
-import AppBar from "./_components/appbar";
 
 export const metadata = {
   title: "Social Media App",
@@ -21,10 +20,7 @@ export default function RootLayout({
         {/* Main content */}
         <main className=" container max-w-7xl mx-auto px-4 py-4 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-6">
           <LeftPannel />
-          <div className="md:col-span-2 lg:col-span-2">
-            <AppBar />
-            {children}
-          </div>
+          <div className="md:col-span-2 lg:col-span-2">{children}</div>
           {/* Right sidebar - Chess Rooms */}
 
           <GameRoomsSidebar />
