2a1ba891cf2276162d9a680225d7b7278b85da0f
Mbaocha Jonathan <mbaocha240793@gmail.com>
2025-07-04 05:40:32 +0100

    feat: update font pairing to use Montserrat and Audiowide

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${montserrat.variable} ${audiowide.variable}`}>
+    <html lang="en" className={`${montserrat.className} ${audiowide.variable}`}>
       <body>
         <ThemeProvider
           attribute="class"
